5458f207ad0b134ea037fd5bfd8dc407a96808cb
Kevin Mitchell <kevin@develpr.com>
2018-05-01 00:25:54 -0500

    manually add cors for now

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,12 @@ app.disable('x-powered-by');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.json({"ok": "ok"});
 });
