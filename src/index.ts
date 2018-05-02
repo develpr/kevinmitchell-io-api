ac259277962614883b2478393d69274ba5413870
Kevin Mitchell <kevin@develpr.com>
2018-05-01 21:29:04 -0500

    Add blog posts and slug

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,8 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.json({"ok": "ok"});
+  res.status(418);
+  res.json({"teapot": true});
 });
 
 app.get('/content/blocks/:id', function(req, res) {
@@ -33,23 +34,77 @@ app.get('/content/blocks/:id', function(req, res) {
     Key: {
       id: req.params.id,
     },
-  }
-  
+  }  
   dynamoDb.get(params, (error: any, result: any) => {
     if (error) {
-      console.log(error);
-      res.status(400).json({ error: 'Could not get user' });
+      res.status(400).json({ error: 'Could not load block.' });
     }
     if (result.Item) {    
       let response = result.Item;
       response.html = marked(response.content);        
-      res.json(result.Item);
+      res.json(response);
     } else {
-      res.status(404).json({ error: "User not found" });
+      res.status(404).json({ error: "Block not found" });
     }
   });
-
 });
+
+
+app.get('/content/blogs/:slug', function(req, res) {
+  const params = {
+    TableName: "kevinmitchell-io-content-blogs",
+    FilterExpression: "slug = :slug",    
+    ExpressionAttributeValues: {
+         ":slug": req.params.slug
+    }
+  }
+  dynamoDb.scan(params, (error: any, result: any) => {
+    if (error) {      
+      res.status(400).json({ error: 'Could not load blog post.' });
+    }
+    
+    if (result.Items && result.Items.length > 0) {          
+      let response = result.Items[0];      
+      response.html = marked(response.post);        
+      res.json(response);
+    } else {
+      res.status(404).json({ error: "Blog post not found" });
+    }
+  });
+});
+
+
+app.get('/content/blogs', function(req, res) {
+
+  let site = req.query.site;
+
+  if( ! site ) {
+    res.status(400).json({ error: "'site' parameter required." });
+    return;
+  }
+  
+  const params = {
+    TableName: "kevinmitchell-io-content-blogs",
+    IndexName: "site-dateCreated-index",    
+    KeyConditionExpression: 'site = :site',
+    ExpressionAttributeValues: {
+      ':site': site
+    },
+    ProjectionExpression: "title,slug,dateCreated",
+    ScanIndexForward: false,    // true = ascending, false = descending    
+  }  
+  dynamoDb.query(params, (error: any, result: any) => {
+    if (error) {
+      res.status(400).json({ error: 'Could not load blog post.' });
+    }    
+    if (result.Items) {          
+      res.json(result.Items);
+    } else {
+      res.status(404).json({ error: "Blog post not found" });
+    }
+  });
+});
+
 
 app.get('/content', function (req, res) {
   res.status(404);
