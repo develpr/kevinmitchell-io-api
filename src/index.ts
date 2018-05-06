dc13bae1852dcf582996e1cd2fd2a7cbf3513b2a
Kevin Mitchell <kevin@develpr.com>
2018-05-05 22:09:46 -0500

    Update to use marked-bear

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,48 @@
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
 import * as express from "express";
-import * as bodyParser from 'body-parser';
-import * as marked from 'marked';
+import * as bodyParser from "body-parser";
+import * as markedBear from "marked-bear";
 
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: "us-west-2"
+  region: "us-west-2"
 });
 
+const app = express();
 
-const app = express()
-
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.get('/', function (req, res) {
+app.get("/", function(req, res) {
   res.status(418);
-  res.json({"teapot": true});
+  res.json({ teapot: true });
 });
 
-app.get('/content/blocks/:id', function(req, res) {
-
+app.get("/content/blocks/:id", function(req, res) {
   const params = {
     TableName: "kevinmitchell-io-content-blocks",
     Key: {
-      id: req.params.id,
-    },
-  }  
+      id: req.params.id
+    }
+  };
   dynamoDb.get(params, (error: any, result: any) => {
     if (error) {
-      res.status(400).json({ error: 'Could not load block.' });
+      res.status(400).json({ error: "Could not load block." });
     }
-    if (result.Item) {    
+    if (result.Item) {
       let response = result.Item;
-      response.html = marked(response.content);        
+      response.html = markedBear(response.content);
       res.json(response);
     } else {
       res.status(404).json({ error: "Block not found" });
@@ -49,23 +50,22 @@ app.get('/content/blocks/:id', function(req, res) {
   });
 });
 
-
-app.get('/content/posts/:slug', function(req, res) {
+app.get("/content/posts/:slug", function(req, res) {
   const params = {
     TableName: "kevinmitchell-io-content-blogs",
-    FilterExpression: "slug = :slug",    
+    FilterExpression: "slug = :slug",
     ExpressionAttributeValues: {
-         ":slug": req.params.slug
+      ":slug": req.params.slug
     }
-  }
+  };
   dynamoDb.scan(params, (error: any, result: any) => {
-    if (error) {      
-      res.status(400).json({ error: 'Could not load blog post.' });
+    if (error) {
+      res.status(400).json({ error: "Could not load blog post." });
     }
-    
-    if (result.Items && result.Items.length > 0) {          
-      let response = result.Items[0];      
-      response.html = marked(response.post);        
+
+    if (result.Items && result.Items.length > 0) {
+      let response = result.Items[0];
+      response.html = markedBear(response.post);
       res.json(response);
     } else {
       res.status(404).json({ error: "Blog post not found" });
@@ -73,33 +73,31 @@ app.get('/content/posts/:slug', function(req, res) {
   });
 });
 
-
-app.get('/content/posts', function(req, res) {
-
+app.get("/content/posts", function(req, res) {
   let site = req.query.site;
 
-  if( ! site ) {
+  if (!site) {
     res.status(400).json({ error: "'site' parameter required." });
     return;
   }
-  
+
   const params = {
     TableName: "kevinmitchell-io-content-blogs",
-    IndexName: "site-dateCreated-index",    
-    KeyConditionExpression: 'site = :site',
+    IndexName: "site-dateCreated-index",
+    KeyConditionExpression: "site = :site",
     ExpressionAttributeValues: {
-      ':site': site
+      ":site": site
     },
     ProjectionExpression: "title,slug,dateCreated,preview",
-    ScanIndexForward: false,    // true = ascending, false = descending    
-  }  
+    ScanIndexForward: false // true = ascending, false = descending
+  };
   dynamoDb.query(params, (error: any, result: any) => {
     if (error) {
-      res.status(400).json({ error: 'Could not load blog post.' });
-    }    
-    if (result.Items && result.Items.length > 0) {          
-      result.Items.map((item:any) => {
-        item.previewHtml = marked(item.preview);
+      res.status(400).json({ error: "Could not load blog post." });
+    }
+    if (result.Items && result.Items.length > 0) {
+      result.Items.map((item: any) => {
+        item.previewHtml = markedBear(item.preview);
         return item;
       });
       res.json(result.Items);
@@ -109,10 +107,13 @@ app.get('/content/posts', function(req, res) {
   });
 });
 
-
-app.get('/content', function (req, res) {
+app.get("/content", function(req, res) {
   res.status(404);
-  res.json({"error": true, "message":"Please check /content/posts or /content/blocks for specific content."});
+  res.json({
+    error: true,
+    message:
+      "Please check /content/posts or /content/blocks for specific content."
+  });
 });
 
 module.exports.handler = serverless(app);
